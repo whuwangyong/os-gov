dcbcba9ecd0be22d9020661cacfa656180a5bc80
whuwangyong <whuwangyong@gmail.com>
2022-09-09 14:09:26 +0800

    打包之后json文件在zip里面，应该使用InputStream读，而非File。否则会报错class path resource [static/nav.json] cannot be resolved to absolute file path because it does not reside in the file system。

--- src/main/java/cn/whu/wy/osgov/controller/NavController.java
+++ src/main/java/cn/whu/wy/osgov/controller/NavController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author WangYong
@@ -24,7 +26,8 @@ public class NavController {
     @GetMapping
     public Object nav() throws IOException, ParseException {
         Resource resource = new ClassPathResource("static/nav.json");
-        File file = resource.getFile();
-        return new JSONParser().parse(new FileReader(file));
+        InputStream inputStream = resource.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        return new JSONParser().parse(bufferedReader);
     }
 }
