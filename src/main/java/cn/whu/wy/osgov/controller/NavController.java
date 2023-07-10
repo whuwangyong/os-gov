f40aa6ecb924cae76c60a30b932df8a7ac0e63b6
whuwangyong <whuwangyong@gmail.com>
2023-07-10 20:20:17 +0800

    优化，避免重复读nav.json文件

--- src/main/java/cn/whu/wy/osgov/controller/NavController.java
+++ src/main/java/cn/whu/wy/osgov/controller/NavController.java
@@ -20,16 +20,26 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping(RequestPath.NAV)
 public class NavController {
 
+    private static String NAV_JSON;
+
+    static {
+        try {
+            ClassPathResource resource = new ClassPathResource("static/nav.json");
+            InputStream inputStream = resource.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            NAV_JSON = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @GetMapping
     public Object nav() throws IOException {
-        ClassPathResource resource = new ClassPathResource("static/nav.json");
-        InputStream inputStream = resource.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            sb.append(line).append(System.lineSeparator());
-        }
-        return sb.toString();
+        return NAV_JSON;
     }
 }
